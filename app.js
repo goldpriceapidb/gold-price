90513fea11974e8772c7222e68bb4d3822ec2e63
tharunoptimus <tharunoptimus@outlook.com>
2022-12-26 00:43:53 +0530

    Added Home page and added cache control

--- app.js
+++ app.js
@@ -2,14 +2,36 @@ import express from "express"
 import cors from "./middleware.js"
 import fetchAndScrape from "./utilities/fetchAndScrape.js"
 
+import path from 'path'
+const __dirname = path.resolve()
+
 let app = express()
 let port = process.env.PORT || 3003
+
+let goldRate = {}
 
 app.listen(port, () => {
 	console.log(`Alive on port ${port}`)
 })
 
 app.get("/", cors, async (_, res) => {
-	let data = await fetchAndScrape()
-	res.status(200).send(data)
+	res.sendFile(__dirname + "/index.html")
 })
+
+app.get("/rate", (_, res) => {
+	res.status(200).send(goldRate)
+})
+
+app.get("/update", cors, async (_, res) => {
+	await update()
+	res.status(200).send("UPDATED GOLD RATE")
+})
+
+async function update() {
+	goldRate = await fetchAndScrape()
+}
+
+update()
+
+let thirtyMinutes = 1000 * 60 * 30
+setInterval(update, thirtyMinutes)
