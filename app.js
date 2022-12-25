c13e8fa4d4d24326b6f946f9844da1771a12a917
tharunoptimus <tharunoptimus@outlook.com>
2022-12-26 00:32:46 +0530

    Cyclic didn't support `read-write` file system

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 import express from "express"
 import cors from "./middleware.js"
 import fetchAndScrape from "./utilities/fetchAndScrape.js"
-import saveToFile from "./store/save.js"
-import { indianGoldPrice } from "./store/save.js"
 
 let app = express()
 let port = process.env.PORT || 3003
@@ -12,21 +10,6 @@ app.listen(port, () => {
 })
 
 app.get("/", cors, async (_, res) => {
-	res.status(200).sendFile(indianGoldPrice)
-})
-
-app.get("/live", cors, async (_, res) => {
 	let data = await fetchAndScrape()
 	res.status(200).send(data)
 })
-
-app.get("/save", cors, async (_, res) => {
-	await saveToFile()
-	res.status(200).send("Saved to file")
-})
-
-let thirtyMinutes = 1000 * 60 * 30
-
-setInterval(async () => {
-	await saveToFile()
-}, thirtyMinutes)
