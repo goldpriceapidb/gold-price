0e07f0a71832c9cc38e1e052c010797b4940d17a
tharunoptimus <tharunoptimus@outlook.com>
2023-01-15 22:49:10 +0530

    Added a field in Country Schema

--- schemas/CountrySchema.js
+++ schemas/CountrySchema.js
@@ -5,11 +5,15 @@ const CountrySchema = new Schema({
     countryName: { type: String, required: true, trim: true },
     countryCode: { type: String, required: true, trim: true },
     currency: { type: String, required: true, trim: true },
+
     previousPrice: { type: Number, required: true },
     currentPrice: { type: Number, required: true },
+
     priceChange: { type: Number, required: true },
     priceChangePercentage: { type: Number, required: true },
+
     serverLastUpdated: { type: Date, required: true },
+    currencyLastUpdated: { type: Date, required: true },
 }, { timestamps: true })
 
 let Country = mongoose.model('Country', CountrySchema)
