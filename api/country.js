eb0c5866885234dbf1eda6b409dd272d4c85e35f
tharunoptimus <tharunoptimus@outlook.com>
2023-01-15 17:38:34 +0530

    Added `code` and `all` route for Country API

--- api/country.js
+++ api/country.js
@@ -1,4 +1,5 @@
 import express from "express"
+import Country from "../schemas/CountrySchema.js"
 
 const app = express()
 const router = express.Router()
@@ -7,7 +8,27 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 router.get("/", (_, res) => {
-	res.send({ message: "country get" })
+	res.send({ message: "COUNTRY API ONLINE" })
 })
+
+router.get("/all", async (_, res) => {
+    try {
+        const countries = await Country.find({})
+        res.status(200).send(countries)
+    } catch (error) {
+        res.status(500).send({ error })
+    }
+})
+
+router.get("/code/:code", async (req, res) => {
+    const { code } = req.params
+    try {
+        const country = await Country.findOne({ countryCode: code })
+        res.status(200).send(country)
+    } catch (error) {
+        res.status(500).send({ error })
+    }
+})
+
 
 export default router
