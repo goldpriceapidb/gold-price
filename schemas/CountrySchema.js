5de75f9f5e4216b00020315f886b8bf6ceb50071
tharunoptimus <tharunoptimus@outlook.com>
2023-01-15 17:13:58 +0530

    Added country schema

---
+++ schemas/CountrySchema.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose"
+const Schema = mongoose.Schema
+
+const CountrySchema = new Schema({
+    countryName: { type: String, required: true, trim: true },
+    countryCode: { type: String, required: true, trim: true },
+    currency: { type: String, required: true, trim: true },
+    previousPrice: { type: Number, required: true },
+    currentPrice: { type: Number, required: true },
+    priceChange: { type: Number, required: true },
+    priceChangePercentage: { type: Number, required: true },
+    serverLastUpdated: { type: Date, required: true },
+}, { timestamps: true })
+
+let Country = mongoose.model('Country', CountrySchema)
+export default Country
