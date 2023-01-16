19e6ff8e157e25f9e02aaac8fb7dc75818c9f681
tharunoptimus <tharunoptimus@outlook.com>
2023-01-16 14:47:17 +0530

    Added `getGoldPriceAndCurrency`

--- utilities/liveprice.js
+++ utilities/liveprice.js
@@ -1,1 +1,17 @@
- 
+
+function getGoldPriceAndCurrency(content) {
+	let $ = cheerio.load(content)
+	let img = $(".cleft.fleft.data-img.pad3")
+	let parent = img.parent()
+	let table = parent.find(".data-table-2")
+	let rows = table.find("tbody tr")
+	let row = $(rows[1])
+	let cols = row.find("td")
+
+	let noisyCurrencyText = $(cols[1]).text().trim()
+	let goldRate = $(cols[2]).text().trim()
+	return {
+		goldRate: sanitizeGoldRate(goldRate),
+		currency: sanitizeTextToCurrency(noisyCurrencyText)
+	}
+}
